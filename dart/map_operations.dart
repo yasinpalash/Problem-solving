e4a9540f4c60af7fcb3aaf09e91cefe02db2af7e
yasin <yasinmiapolash@gmail.com>
2024-07-31 00:32:17 +0600

    map

---
+++ dart/map_operations.dart
@@ -0,0 +1,7 @@
+void main() {
+  // 1. Creating a map of students and their scores
+  var studentScores = {'Yasain': 75, 'bod': 99, 'chk': 23};
+// 2. Adding a new student's score
+  studentScores['David'] = 37;
+  print('added David:$studentScores');
+}
