92cb2b1a0f313656f694b292fb7d59b00856b42e
yasin <yasinmiapolash@gmail.com>
2024-08-05 00:04:22 +0600

    List

--- dart/list_operations.dart
+++ dart/list_operations.dart
@@ -19,4 +19,60 @@ void main() {
   // 7. Updating all scores (e.g., adding a bonus of 5 points to each)
   var updatedScores = studentScores.map((e) => e + 5).toList();
   print('Added bonus to all scores: $updatedScores');
+
+  // 8. Removing a score
+  studentScores.remove(90);
+  print('Removed score 90: $studentScores');
+  // 9. Removing a score at a specific position
+  studentScores.removeAt(3);
+  print('Removed score at index 3: $studentScores');
+  // 10. Removing multiple scores by their position
+  studentScores.removeRange(2, 4);
+  print('Removed scores from index 2 to 4: $studentScores');
+  // 11. Removing scores conditionally (e.g., remove scores less than 85)
+  studentScores.removeWhere((element) => element < 85);
+  print('Removed scores less than 85: $studentScores');
+
+  // 12. Accessing a score by index
+  var studentIndex2 = studentScores[2];
+  print('Score at index 2: $studentIndex2');
+  // 13. Checking if a score exists in the list
+
+  var check88 = studentScores.contains(88);
+  print('Contains score 88: $check88');
+  // 14. Iterating over scores
+  print('All scores:');
+  for (var score in studentScores) {
+    print(score);
+  }
+  // 15. Getting the number of scores
+  var numberOfLength = studentScores.length;
+  print('Number of scores: $numberOfLength');
+  // 16. Checking if the list is empty
+  var isEmpty = studentScores.isEmpty;
+  print('Is the list empty: $isEmpty');
+  // 17. Checking if the list is not empty
+  var isNotEmpty = studentScores.isNotEmpty;
+  print('Is the list not empty: $isNotEmpty');
+  // 18. Sorting the scores
+  studentScores.sort();
+  print('Sorted scores: $studentScores');
+  // 19. Reversing the list of scores
+  var reversedScores = studentScores.reversed.toList();
+  print('Reversed scores: $reversedScores');
+  // 20. Clearing all scores
+  studentScores.clear();
+  print('Cleared all scores: $studentScores');
+
+  // 21. Creating a list from another list
+  var newScores = List.from([44, 55, 66]);
+  print('New scores from another list: $newScores');
+
+  // 22. Creating a list with a fixed length
+  var fixedLengthList = List<int>.filled(5, 0);
+  print('Fixed length list: $fixedLengthList');
+
+  // 23. Creating a list from an iterable
+  var iterableList = List<int>.from([1, 2, 3, 4, 5]);
+  print('List from iterable: $iterableList');
 }
