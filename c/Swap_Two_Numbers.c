25ae6da14092a668cfb33d48d9e6f290fd377ddf
yasin <yasinmiapolash@gmail.com>
2025-04-11 23:34:18 +0600

    Refactor code structure for improved readability and maintainability

--- c/Swap_Two_Numbers.c
+++ c/Swap_Two_Numbers.c
@@ -1,1 +1,17 @@
 // Problem  Write a C program to swap two numbers without using a temporary variable.
+#include <stdio.h>
+
+int main()
+{
+    int x = 6, y = 10;
+    printf("Before swap: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("After swap: x = %d, y = %d\n", x, y);
+}
+
+int swap(int *a, int *b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
