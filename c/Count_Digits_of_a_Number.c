c8fa3f1482246d6b66c4005e0835e5da7ee64ea7
yasin <yasinmiapolash@gmail.com>
2025-04-11 21:45:40 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ c/Count_Digits_of_a_Number.c
@@ -0,0 +1,21 @@
+// Problem : Write a C program to count the number of digits in a number.
+#include <stdio.h>
+
+int main()
+{
+
+    int num = 1233;
+    printf("Number of digits: %d\n", countDigits(num));
+    return 0;
+}
+
+int countDigits(int num)
+{
+    int count = 0;
+    while (num != 0)
+    {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
